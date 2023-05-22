057d36e64cd0fe0383379424b5dedf2b4fcb7574
NVD2352001 <119424631+NVD2352001@users.noreply.github.com>
2023-05-22 14:39:20 +0700

    Add files via upload

---
+++ TQQ.com/assets/a.js
@@ -0,0 +1,13 @@
+/*var number = 10
+
+var a = ++number * 3 - number-- * 2
+var b = number++ * 2 - --number * 3
+
+console.log(a + b) // ???*/
+/*
+number=10 
+++number =10 11
+number++ =11 11*/
+var date = new Date();
+var day = date.getDate();
+console.log(day);
